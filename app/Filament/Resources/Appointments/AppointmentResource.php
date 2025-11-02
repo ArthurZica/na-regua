e8e3b3a2a5bf91d9a19314aafe14595f1fc33414
Arthur Zica <arthur.zica@aglets.com.br>
2025-11-02 18:34:03 -0300

    Fix: icone agendamentos.

--- app/Filament/Resources/Appointments/AppointmentResource.php
+++ app/Filament/Resources/Appointments/AppointmentResource.php
@@ -20,7 +20,7 @@ class AppointmentResource extends Resource
 {
     protected static ?string $model = Appointment::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::Calendar;
 
     public static function getModelLabel(): string
     {
